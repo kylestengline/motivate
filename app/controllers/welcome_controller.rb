ad26e76378565fc743b5275a9fd96bfe32ca5f9a
Kyle Stengline <kyle.stengline@gmail.com>
2016-02-29 20:20:15 -0500

    posts show on index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 	
 	def index
+		# @user_name = User.where(user_id.user_name)
+		# Want the users user name to show here.
+		@posts = Post.all
 	end
 
 end

669766f9ee23a8945b8367289a83d7f8d20fc2c7
Kyle Stengline <kyle.stengline@gmail.com>
2016-03-11 16:41:45 -0500

    mailers for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,18 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'motivates.herokuapp.com',
+    user_name: ENV['ADMINISTRATOR_EMAIL'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 

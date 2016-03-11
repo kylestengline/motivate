75e0b08eaae7791fe55e379d77ded37bf209fd44
Kyle Stengline <kyle.stengline@gmail.com>
2016-03-11 16:45:00 -0500

    adds mailers to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'motivates.herokuapp.com',
+    domain: 'gmail.com',
     user_name: ENV['ADMINISTRATOR_EMAIL'],
     password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',

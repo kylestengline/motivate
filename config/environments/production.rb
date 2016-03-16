74cdeac2b94c00090fe377df3b618b64df721580
Kyle Stengline <kyle.stengline@gmail.com>
2016-03-16 14:50:56 -0400

    adds cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  config.action_cable.url = 'wss://motivates.herokuapp.com/rooms/show/cable'
+  config.action_cable.url = 'wss://motivates.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://motivates.herokuapp.com', /http:\/\/motivates.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.

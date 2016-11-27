8a9ccea407182c4c6f444819e24a796c8ab1c6a4
Kyle Stengline <kyle.stengline@gmail.com>
2016-11-27 14:07:02 -0500

    fixes logout bug and adds sessions helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def current_user
-    case session[:user_type]
-      when 'Administrator' then Administrator.find_by(id: session[:user_id])
-      #when 'User' then User.find_by(id: session[:user_id])
-    end
-  end
+  include SessionsHelper
 
   def logout 
-    session.delete(:user_id)
-    @current_user = nil
-    #reset_session
+    session.delete('user_id')
+    redirect_to root_path
   end
 end

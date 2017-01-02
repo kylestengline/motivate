d9323c73ef3ce39058a72a50ecc016ed52cf59ad
Kyle Stengline <kyle.stengline@gmail.com>
2017-01-02 10:41:44 -0500

    passes creating posts, adds main html for flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,14 +31,12 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      flash[:notice] = 'Post was successfully created.'
+      redirect_to posts_path
+    else
+      flash.now[:danger] = "Post not created"
+      render :new
     end
   end
 

f0463e0fd61fe4db286490339223ca0f0101a270
Kyle Stengline <kyle.stengline@gmail.com>
2017-01-14 21:19:08 -0800

    adds spec test helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,18 +8,8 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
 require 'database_cleaner'
+require 'support/spec_test_helper'
 # Add additional requires below this line. Rails is not loaded until this point!
-
-def login(user)
-  user = Administrator.create(name: "My Name", email: "my@example.com", password: "password" )
-  visit root_path
-
-  click_link "Admin Login"
-
-  fill_in "Email", with: user.email
-  fill_in "Password", with: user.password
-  click_on "Log Me In. Please and Thanks!"
-end
 
 RSpec.configure do |config|
 
--- spec/support/spec_test_helper.rb
+++ spec/support/spec_test_helper.rb
@@ -1,7 +1,12 @@
 require 'capybara/rails'
 
-module SpecTestHelper
-  def login_as(user)
-    user.login
-  end
+def login(user)
+  user = Administrator.create(name: "My Name", email: "my@example.com", password: "password" )
+  visit root_path
+
+  click_link "Admin Login"
+
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_on "Log Me In. Please and Thanks!"
 end

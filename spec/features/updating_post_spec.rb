71a5690596f069316dd0d9b646e2b75c4b09e677
Kyle Stengline <kyle.stengline@gmail.com>
2017-01-29 11:15:50 -0800

    passes update spec

--- spec/features/updating_post_spec.rb
+++ spec/features/updating_post_spec.rb
@@ -12,14 +12,32 @@ RSpec.feature "Updating Posts" do
 
     click_on "Edit"
 
-    expect(current_path).to eq(edit_admin_post_path(post))
+    expect(page).to have_current_path(edit_admin_post_path(post))
 
     fill_in "Content", with: "Something New"
     fill_in "Story", with: "A different tale than the previous"
     fill_in "Author", with: "Same Author"
     click_on "Update Post"
 
-    expect(page.current_path).to eq(admin_post_path(post))
     expect(page).to have_content "Post was successfully updated."
+    expect(page).to have_current_path(admin_post_path(post))
+  end
+
+  scenario "admins can update posts" do
+    login(administrator)
+
+    click_link "Admin Page"
+
+    click_on "Edit"
+
+    expect(page).to have_current_path(edit_admin_post_path(post))
+
+    fill_in "Content", with: ""
+    fill_in "Story", with: ""
+    fill_in "Author", with: ""
+    click_on "Update Post"
+
+    expect(page).to have_content "Post not updated"
+    expect(page).to have_current_path(admin_post_path(post))
   end
 end
